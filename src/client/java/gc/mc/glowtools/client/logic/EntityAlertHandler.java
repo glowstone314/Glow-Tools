2d471e5a858fff168e932cf5ddfbbeb7ffbde921
Glow_Creeper <88934769+glowstone314@users.noreply.github.com>
2026-04-29 18:15:00 +0800

    Allow not installing Tweakeroo; Set the value to false after triggering EntityAlerts.EXIT_WORLD; Internal sound id / external sound path configured separately.

--- src/client/java/gc/mc/glowtools/client/logic/EntityAlertHandler.java
+++ src/client/java/gc/mc/glowtools/client/logic/EntityAlertHandler.java
@@ -98,7 +98,9 @@ public class EntityAlertHandler {
 
                     client.world.disconnect(title);
                     client.disconnect(new DisconnectedScreen(new TitleScreen(), title, reason), true);
-                    //client.setScreen();
+
+                    Configs.EntityAlerts.EXIT_WORLD.setBooleanValue(false);
+                    Configs.saveToFile();
                 }
             });
         }
