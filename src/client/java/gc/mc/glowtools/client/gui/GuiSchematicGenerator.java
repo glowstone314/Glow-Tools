556c69d36fb85ec7dedf7802f3fdcb187b599fae
Glow_Creeper <88934769+glowstone314@users.noreply.github.com>
2026-07-07 20:34:26 +0800

    Add Litematica mod security vulnerability warning.

--- src/client/java/gc/mc/glowtools/client/gui/GuiSchematicGenerator.java
+++ src/client/java/gc/mc/glowtools/client/gui/GuiSchematicGenerator.java
@@ -10,6 +10,9 @@ import gc.mc.glowtools.client.config.ShapeMode;
 import gc.mc.glowtools.client.compat.LitematicaBridge;
 import gc.mc.glowtools.client.util.math.ShapeStrategy;
 import gc.mc.glowtools.client.util.math.impl.*;
+import net.fabricmc.loader.api.FabricLoader;
+import net.fabricmc.loader.api.Version;
+import net.fabricmc.loader.api.VersionParsingException;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.client.MinecraftClient;
@@ -74,6 +77,14 @@ public class GuiSchematicGenerator extends GuiBase {
             this.addButton(backButton, (button, mouseButton) -> GuiBase.openGui(new GuiConfigs()));
             return;
         }
+
+        Version v = FabricLoader.getInstance().getModContainer("litematica").get().getMetadata().getVersion();
+        try {
+            int compare = v.compareTo(Version.parse("0.26.11"));
+            if (compare < 0) {
+                this.addMessage(MessageType.WARNING, "glowtools.warning.schematic_generator.litematicaVersion");
+            }
+        } catch (VersionParsingException ignored) {}
 
         int x = 12;
         int y = 30;
